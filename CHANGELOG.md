726a28592e0de1ef3d76dbc0dc859196c6ff21f7
Istvan Soos <istvan.soos@agilord.com>
2018-04-15 09:27:27 +0200

    Fix analyzer issues.

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.0.2
+
+- Updated for Dart 2
---
+++ example/example.dart
@@ -0,0 +1,6 @@
+import 'package:diacritic/diacritic.dart';
+
+void main() {
+  // prints 'arvizturo tukorfurogep'
+  print(removeDiacritics('árvíztűrő tükörfúrógép'));
+}
